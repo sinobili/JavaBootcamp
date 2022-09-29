73e59fb57445ab0d02c50ecad2235c8fcf39a2a9
Sinobili <38077840+sinobili@users.noreply.github.com>
2022-09-29 21:55:24 +0300

    week2

---
+++ javademos/week2/classesWithAttributes/src/classesWithAttributes/Main.java
@@ -0,0 +1,22 @@
+package classesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product = new Product();
+		product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Asus Laptop");
+		product.setPrice(5000);
+		product.setStockAmount(3);
+		product.getKod();
+		
+		System.out.println(product.getKod());
+		
+		ProductManager productManager = new ProductManager();
+		productManager.Add(product);
+		
+
+	}
+
+}
---
+++ javademos/week2/inheritance/src/inheritance/EmployeeManager.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class EmployeeManager {
+
+	
+	public void BestEmployee() {
+		System.out.println("Ayın elemanı getirildi");
+	}
+	
+}
